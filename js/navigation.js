09e2f607110b6a765fabe0deb43327d5167054b4
unknown <ritwikmathhithub@gmail.com>
2019-07-17 01:47:01 +0530

    style home

---
+++ js/navigation.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  var scroll_pos = 0;
+  $(document).scroll(function() {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 210) {
+      $(".navbar").css("background", "rgba(255, 255, 255, 0.6)");
+    } else {
+      $(".navbar").css("background", "white");
+    }
+  });
+});
+$(".slogan").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
